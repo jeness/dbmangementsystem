d00cc86cb929df5eb71b5badabcb9e94e4d17a1b
jeness <yuhaoran9968@gmail.com>
2018-04-15 17:32:54 -0400

    oci connect with oracle and php

--- oracletest.php
+++ oracletest.php
@@ -1,4 +1,5 @@
-<?php
+
+<!-- <?php
 
 $conn = oci_connect('YYU', '31415Zhang', 'oracle.cise.ufl.edu:1521/orcl');
 if (!$conn) {
@@ -12,7 +13,7 @@ oci_fetch_all($stid, $res);
 var_dump($res);
 
 
-$stid1 = oci_parse($conn, 'SELECT * FROM "checkout" where rownum<=5');
+$stid1 = oci_parse($conn, 'SELECT * FROM "CHECKOUTRECORD" where rownum<=5');
 $r = oci_execute($stid1);
 oci_fetch_all($stid1, $res1);
 var_dump($res1);
@@ -22,4 +23,46 @@ oci_free_statement($stid);
 oci_free_statement($stid1);
 oci_close($conn);
 
-?>
+?> -->
+
+
+ <?php       
+        //设置绑定变量的取值
+       $cardNumber="1234";
+       $name = "101234";
+       $department = "1234";
+       $password = "1234";
+
+       $conn = oci_connect('YYU', '31415Zhang', 'oracle.cise.ufl.edu:1521/orcl');
+       if (!$conn) {
+             $e = oci_error();
+             trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+       }
+       // $query = "INSERT INTO card
+       //  VALUES(:card_number,:name,:department,:password)";
+      $stid = oci_parse($conn, 'SELECT * FROM "card" where rownum<=5');
+      $r = oci_execute($stid);
+      oci_fetch_all($stid, $res);
+      var_dump($res);
+      oci_free_statement($stid);
+
+       $statement=oci_parse($conn, 'INSERT INTO "card" VALUES(:card_number,:name,:department,:password)');
+
+        oci_bind_by_name($statement,":card_number",$cardNumber);
+		oci_bind_by_name($statement, ':name', $name);
+		oci_bind_by_name($statement,':department',$department);
+		oci_bind_by_name($statement,':password', $password);
+       //执行语句
+       $resul = oci_execute($statement);
+       // //取得结果数据
+       // oci_fetch_all($statement, $res1);
+       // foreach($res1 as $rows){
+       //     echo " ";
+       //     foreach($rows as $col_values){
+       //     echo $col_values;   
+       //     }
+       // }
+       //释放资源
+       oci_free_statement($statement);
+       oci_close($conn);
+       ?>
--- registCard.php
+++ registCard.php
@@ -1,26 +1,29 @@
 <?php
-	$con = mysql_connect("localhost","root","");
+	$con = oci_connect('YYU', '31415Zhang', 'oracle.cise.ufl.edu:1521/orcl');
 	session_start();
 	if (!$con)
 	{
-	  die('Could not connect: ' . mysql_error());
+	  die('Could not connect: ' . oci_error());
 	}
-	mysql_select_db("my_db", $con);
+	// mysql_select_db("my_db", $con);
 
 	$cardNumber = $_GET['CardNumber'];
 	$name 		= $_GET['Name'];
 	$department = $_GET['Department'];
 	$password 	= $_GET['PassWord'];
-
-	$searchCard = "
-		select *
+	// $searchCard = "
+	// 	select *
+	// 	from card
+	// 	where card.card_number = \"" .$cardNumber.
+	// 	"\"";
+	$statement = oci_parse($con,'select *
 		from card
-		where card.card_number = \"" .$cardNumber.
-		"\"";
-
-	$query = mysql_query($searchCard);
+		where card.card_number = (:cardNumber)');
+	oci_bind_by_name($statement,":cardNumber",$cardNumber);
+	oci_execute($statement);
+	// $query = mysql_query($searchCard);
 	$countCard = 0;
-	while($row = mysql_fetch_array($query))
+	while($row = oci_fetch_array($statement))
   	{
   		$countCard = $countCard + 1;
   	}
@@ -32,16 +35,22 @@
 		echo "</script>";
   	}
   	else {
-  		$sqlQuery = " 	
-			insert into card
-			values (\"" .$cardNumber. "\", \"" .$name. "\", \"" .$department. "\", \"" .$password. "\")
-		";
-			$query = mysql_query($sqlQuery);
-		$sqlQuery = "
-		    insert into admin ( username, password)
-			values (\"".$cardNumber."\",\"".$password."\")
-        ";
-			$query = mysql_query($sqlQuery);
+  // 		$sqlQuery = " 	
+		// 	insert into card
+		// 	values (\"" .$cardNumber. "\", \"" .$name. "\", \"" .$department. "\", \"" .$password. "\")
+		// ";
+			// $query = mysql_query($sqlQuery);
+			$sqlQuery = "insert into card(card_number, name, department, password) values(:card_number,:name,:department,:passWord )";
+			$statement = oci_parse($con,$sqlQuery);
+			oci_bind_by_name($statement,':card_number',$cardNumber);
+			oci_bind_by_name($statement, ':name', $name);
+			oci_bind_by_name($statement,':department',$department);
+			oci_bind_by_name($statement,':password', $password);
+			oci_execute($statement,OCI_COMMIT_ON_SUCCESS);
+		    oci_free_statement($statement);  
+		    if(oci_num_rows){  
+            echo "插入成功";  
+      	    }  
 		//echo $sqlQuery;
 		
 		//echo $query;
@@ -49,5 +58,7 @@
 		echo "<script language=\"javascript\">";
 		echo "document.location=\"index.php\"";
 		echo "</script>";
+		oci_close($con);
+
   	}
 ?>
